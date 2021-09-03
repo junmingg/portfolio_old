7cbc0a7e9605d61e6742fb619fc09bccfd242890
junmingg <chenjm246@gmail.com>
2021-09-03 09:43:41 +0800

    Changing the boiler plate template into something simple as a test.

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1> This is the home page for Jun Ming's Portfolio. </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
