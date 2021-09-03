4f51a0bf3d2f9dd6ff124c43a95624547722f5a1
junmingg <chenjm246@gmail.com>
2021-09-03 11:42:58 +0800

    Added an animation to the home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1> This is the home page for Jun Ming's Portfolio. </h1>
+    <body>
+      <div class="wrapper">
+        <div class="static-txt">Hello! I'm a</div>
+        <ul class="dynamic-txts">
+          <li><span>Data Analyst &nbsp; &nbsp;</span></li>
+          <li><span>Data Scientist &nbsp;</span></li>
+          <li><span>ML/AI Engineer</span></li>
+          <li><span>Web Developer</span></li>
+        </ul>
+      </div>
+    </body>
   </div>
 </template>
 
@@ -13,3 +24,75 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+  display: flex;
+  align-items: center;
+   justify-content: center;
+   min-height: 100vh;
+}
+
+.wrapper {
+  display: inline-flex;
+}
+
+.wrapper .static-txt {
+  font-size: 60px;
+  font-weight: 400;
+  padding: 10px;
+}
+
+.wrapper .dynamic-txts {
+  margin-left: 15px;
+  height: 90px;
+  line-height: 90px;
+  overflow: hidden;
+}
+
+.dynamic-txts li {
+  color: #BB86FC;
+  list-style: none;
+  font-size: 60px;
+  font-weight: 500;
+  position: relative;
+  top: 0; 
+  animation: slide 6s steps(4) infinite;
+}
+
+@keyframes slide {
+  100%{
+    top: -360px;
+  }
+}
+
+.dynamic-txts li span {
+  position: relative;
+}
+
+.dynamic-txts li span::after{
+  content: "";
+  position: absolute;
+  left: 0;
+  height: 120%;
+  width: 100%;
+  background: #121212;
+  border-left: 2px solid #BB86FC;
+  animation: typing 1.5s steps(14) infinite;
+}
+
+@keyframes typing {
+  100%{
+    left: 100%;
+    margin: 0 -35px 0 35px;
+  }
+}
+
+</style>
