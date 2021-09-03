07133323e1c106badc5145f2fc88f3e3ff2d6d1c
junmingg <chenjm246@gmail.com>
2021-09-03 11:56:34 +0800

    Added some content to the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Me</h1>
+    <p>I am a Final Year Economics Student with a Second Major in Data Science &amp; Analytics from Singapore Management University. My main interests are in Data Analytics, Data Science, Machine Learning, Artificial Intelligence and Web Development.</p>
+    <p>In my Freshman Year, I was the Safety and Operations Director of SMU Judo and in my Sophomore Year, I was the Vice-President of SMU Judo.</p>
   </div>
 </template>
